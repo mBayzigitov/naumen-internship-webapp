1ee942040b3fa5bb27c6cb85cc150af7e84dc28b
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2023-04-25 22:58:57 +0300

    upd: Routing array filled

--- naumen-ang-app/src/app/app-routing.module.ts
+++ naumen-ang-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PeopleListComponent} from "./people-list/people-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'people', component: PeopleListComponent},
+  {path: '', redirectTo: 'people', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
