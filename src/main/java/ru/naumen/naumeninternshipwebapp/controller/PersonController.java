385ad387bef78f735e6f51a7340c7b2550136190
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2023-04-26 02:18:58 +0300

    upd: Autowiring via constructor added; fix: Post mapping path fixed

--- src/main/java/ru/naumen/naumeninternshipwebapp/controller/PersonController.java
+++ src/main/java/ru/naumen/naumeninternshipwebapp/controller/PersonController.java
@@ -1,23 +1,42 @@
 package ru.naumen.naumeninternshipwebapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.naumen.naumeninternshipwebapp.model.Person;
 import ru.naumen.naumeninternshipwebapp.repository.PersonRepository;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Scanner;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/v1/")
 public class PersonController {
 
-    @Autowired
     private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @GetMapping("/people")
     public List<Person> addPerson() {
         return personRepository.findAll();
+    }
+
+    @PostMapping("/file-uploading")
+    public String recieveFile(@RequestParam("file") MultipartFile file) throws IOException {
+        Scanner scanner = new Scanner(file.getInputStream());
+        System.out.println(scanner.nextLine());
+
+        return "File is uploaded successfully";
     }
 
 }
