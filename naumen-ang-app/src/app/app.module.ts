e2ad31717e255b74edaab7f6dbe3858c75598d46
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2023-04-25 22:20:45 +0300

    upd: Angular folder added

---
+++ naumen-ang-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeopleListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ naumen-ang-app/src/app/people-list/people-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Person } from "../person";
+import {PersonService} from "../person.service";
+
+@Component({
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
+})
+export class PeopleListComponent {
+
+  people!: Person[];
+
+  constructor(private personService: PersonService) {
+  }
+
+  ngOnInit(): void {
+    this.getPeople();
+  }
+
+  private getPeople() {
+    this.personService.getPeopleList().subscribe(data => {
+      this.people = data;
+    })
+  }
+}
