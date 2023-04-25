c42ea56235344240b12988f9d895664190503b1b
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2023-04-25 21:56:46 +0300

    upd: Request mapping changed; fix: CORS block issue fixed

--- src/main/java/ru/naumen/naumeninternshipwebapp/controller/PersonController.java
+++ src/main/java/ru/naumen/naumeninternshipwebapp/controller/PersonController.java
@@ -2,28 +2,26 @@ package ru.naumen.naumeninternshipwebapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.naumen.naumeninternshipwebapp.model.Person;
 import ru.naumen.naumeninternshipwebapp.repository.PersonRepository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("")
+@CrossOrigin("*")
+@RequestMapping("/api/v1/")
 public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
 
-    @GetMapping("")
+    @GetMapping("/people")
     public List<Person> addPerson() {
-        personRepository.save(new Person("Mike", 21));
-        return new ArrayList<>(Collections.singletonList(new Person("Mike", 21)));
+        return personRepository.findAll();
     }
 
 }
