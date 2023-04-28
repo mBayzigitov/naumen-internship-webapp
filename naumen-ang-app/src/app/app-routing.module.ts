f22aa840f0dc4d500357e720eb49cdb78a2572ae
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2023-04-28 23:03:14 +0300

    upd: Stats component added to routing

--- naumen-ang-app/src/app/app-routing.module.ts
+++ naumen-ang-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {PeopleListComponent} from "./people-list/people-list.component";
 import {UploadPageComponent} from "./upload-page/upload-page.component";
 import {NameInputComponent} from "./name-input/name-input.component";
+import {StatsComponent} from "./stats/stats.component";
 
 const routes: Routes = [
   {path: 'people', component: PeopleListComponent},
   {path: 'upload', component: UploadPageComponent },
   {path: '', redirectTo: 'upload', pathMatch: 'full'},
-  {path: 'name-input', component: NameInputComponent}
+  {path: 'name-input', component: NameInputComponent},
+  {path: 'stats', component: StatsComponent}
 ];
 
 @NgModule({
